ebeb6885f369a80079634268b4d263dbdd6be842
Seun Ore <seunsamore@gmail.com>
2021-05-26 10:52:11 +0100

    first shipment

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "gcs" {
+    bucket      = "roava-io-terraform"
+    prefix      = "terraform-circleci/state"
+    credentials = ".keys/gcp_account.json"
+  }
+}
+provider "google" {
+  credentials = ".keys/gcp_account.json"
+  project     = "roava-io"
+  region      = "us-central1"
+}
+
+resource "google_container_cluster" "gke-cluster" {
+  name                     = "demo"
+  location                 = "us-central1"
+  remove_default_node_pool = true
+  # In regional cluster (location is region, not zone) 
+  # this is a number of nodes per zone 
+  initial_node_count = 1
+}
+
+resource "google_container_node_pool" "preemptible_node_pool" {
+  name     = "default-pool"
+  location = "us-central1"
+  cluster = google_container_cluster.gke-cluster.name
+  # In regional cluster (location is region, not zone) 
+
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+    oauth_scopes = [
+      "storage-ro",
+      "logging-write",
+      "monitoring"
+    ]
+  }
+}
