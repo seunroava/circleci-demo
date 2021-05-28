ead9f50993bafbb4bd5c5cc7d1335c22398c003d
Seun Ore <seunsamore@gmail.com>
2021-05-28 13:06:13 +0100

    fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,15 @@
 terraform {
   required_version = ">= 0.12"
+
   backend "gcs" {
     bucket      = "roava-io-terraform"
     prefix      = "terraform-circleci/state"
-    credentials = "../.keys/gcp_account.json"
+    credentials = ".keys/gcp_account.json"
   }
 }
+
 provider "google" {
-  credentials = "../.keys/gcp_account.json"
+  credentials = ".keys/gcp_account.json"
   project     = "roava-io"
   region      = "us-central1"
 }
@@ -16,6 +18,7 @@ resource "google_container_cluster" "gke-cluster" {
   name                     = "demo"
   location                 = "us-central1"
   remove_default_node_pool = true
+
   # In regional cluster (location is region, not zone) 
   # this is a number of nodes per zone 
   initial_node_count = 1
@@ -24,18 +27,19 @@ resource "google_container_cluster" "gke-cluster" {
 resource "google_container_node_pool" "preemptible_node_pool" {
   name     = "default-pool"
   location = "us-central1"
-  cluster  = google_container_cluster.gke-cluster.name
+
+  #cluster  = google_container_cluster.gke-cluster.name
   # In regional cluster (location is region, not zone) 
 
   node_count = 1
-
   node_config {
     preemptible  = true
     machine_type = "n1-standard-1"
+
     oauth_scopes = [
       "storage-ro",
       "logging-write",
-      "monitoring"
+      "monitoring",
     ]
   }
 }
